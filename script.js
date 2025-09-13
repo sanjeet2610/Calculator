307a895b1e5e355de2489a8a2524225fc1a70306
sanjeet2610 <195545915+sanjeet2610@users.noreply.github.com>
2025-09-13 19:57:54 +0000

    Make and store variables and operator

--- script.js
+++ script.js
@@ -20,3 +20,36 @@ function operate(operator, a, b) {
     else if (operator === '/') divide(a, b);
     multiply(a, b);
 }
+let firstNumber;
+let secondNumber;
+let operator;
+let flag = 0;
+
+function populate() {
+    const numbers = document.querySelectorAll(".digit");
+    const operators = document.querySelectorAll(".operators");
+    let n = '';
+    const display = document.querySelector(".display");
+    numbers.forEach(number => {
+        number.addEventListener("click", (e) => {
+            n += e.target.textContent;
+            display.textContent = n;
+        });
+    });
+
+    operators.forEach(operator => {
+        operator.addEventListener("click", (e) => {
+            display.textContent = '';
+            n = '';
+            operator = e.target.textContent;
+            if (flag == 0) {
+                firstNumber = parseInt(display.textContent);
+                flag = 1;
+            } else {
+                secondNumber = parseInt(display.textContent);
+            }
+        });
+    });
+}
+
+populate();
